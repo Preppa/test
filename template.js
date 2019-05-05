371510552ce4692ab31d254c74da6a4dd6d8567a
ryan <ryanli.ryu@gmail.com>
2019-05-05 16:53:02 +0900

    unified header/footer

---
+++ template.js
@@ -0,0 +1,9 @@
+function header(){
+  var html = '<div class="header-left"><h1>Preppa</h1></div>';
+  document.write(html);
+}
+
+function footer(){
+  var html = '<div><p>小さなキレイで毎日を楽しく。</p><h2>Preppa</h2><p>Copyright Preppa All Rights Reserved.</p></div>';
+  document.write(html);
+}
